0356d3c79af0d380dbb0251eb9d729d4efd4167f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:40:52 -0700

    adds conditionals 6 solution

--- conditionals6.js
+++ conditionals6.js
@@ -1,1 +1,10 @@
 //  6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+var x = 100;
+var y = 2;
+
+if (x > 10 || y > 10) {
+  console.log(100);
+} else {
+  console.log(-100);
+}
