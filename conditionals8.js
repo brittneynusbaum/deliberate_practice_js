6d99b032a3b64634b70862971d9ed84ab118fc6b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:43:45 -0700

    adds conditionals 8 solution

--- conditionals8.js
+++ conditionals8.js
@@ -1,1 +1,11 @@
 //  8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+var x = 99;
+
+if (x === 100) {
+  console.log(100);
+} else if (x === 99) {
+  console.log(99);
+} else {
+  console.log(0);
+}
