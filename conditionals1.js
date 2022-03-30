b5524c0e935b37ef0edc2c6ee920decd4f922d20
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:24:12 -0700

    adds conditionals 1 solution

--- conditionals1.js
+++ conditionals1.js
@@ -1,2 +1,9 @@
 // 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
+var x = 10;
+
+if (x === 10) {
+  console.log(0);
+} else {
+  console.log(-1);
+}
