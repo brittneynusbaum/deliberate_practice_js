0a32883eb805957f0c8b039cba7b5881e0d0f1bd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:35:27 -0700

    adds conditionals 4 solution

--- conditionals4.js
+++ conditionals4.js
@@ -1,1 +1,9 @@
 //  4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+var x = 10000;
+
+if (x > 9000) {
+  console.log(1);
+} else {
+  console.log(-1);
+}
