54bafb2a49202b87a26deae39e0ace33aaa0a157
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:45:31 -0700

    adds conditionals 9 solution

--- conditionals9.js
+++ conditionals9.js
@@ -1,1 +1,10 @@
 //  9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+var x = -10;
+var y = 15;
+
+if (x < 0 && y > 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}
