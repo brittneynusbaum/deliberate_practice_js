ee736da700a15edf1b672f76f6e2ce8610ae8e4f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:33:34 -0700

    adds conditionals 3 solution

--- conditionals3.js
+++ conditionals3.js
@@ -1,1 +1,10 @@
 // 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+var x = 1;
+var y = 5;
+
+if (x < 10 && y < 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
