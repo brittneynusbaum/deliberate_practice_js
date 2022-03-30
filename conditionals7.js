d03f5e930ab1d5ff1c689a92fd6ef6d73721524b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-29 23:42:00 -0700

    adds conditionals 7 solution

--- conditionals7.js
+++ conditionals7.js
@@ -1,1 +1,9 @@
 //  7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+var x = -10;
+
+if (x < 0) {
+  console.log(1776);
+} else {
+  console.log(1979);
+}
