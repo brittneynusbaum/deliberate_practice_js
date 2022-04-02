7e42e119faae75fce0b787b9b102d8536d0edd04
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-01 23:16:41 -0700

    adds problem 3

--- strings/strings-practice.js
+++ strings/strings-practice.js
@@ -1,12 +1,19 @@
 // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
-var firstName = "Jessie";
-var lastName = "Stephens";
+// var firstName = "Jessie";
+// var lastName = "Stephens";
 
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
 
 // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+// var firstName = "Jessie";
+// var lastName = "Stephens";
 
 // # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+var word = prompt("Enter a word: ");
+
+if (word === 'marco') {
+  console.log("polo");
+} 
 
 // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
