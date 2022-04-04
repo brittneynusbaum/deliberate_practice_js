27bad440b80cb8cd9ca51799300e7bc8d58b7ba0
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-03 22:36:41 -0700

    adds arrays map practice 1 solution

--- arrays_map/arrays_map_practice.js
+++ arrays_map/arrays_map_practice.js
@@ -1,6 +1,11 @@
 //  1. Start with an array of numbers and create a new array with each number times 3.
 //     For example, [1, 2, 3] becomes [3, 6, 9].
-
+var numbers = [1, 2, 3];
+var timesThree = [];
+numbers.forEach(function(number) {
+  timesThree.push(number * 3);
+});
+console.log(timesThree);
 
 //  2. Start with an array of strings and create a new array with each string upcased.
 //     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
