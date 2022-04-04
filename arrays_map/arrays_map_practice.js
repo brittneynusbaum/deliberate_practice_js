2982e2f7ba78327d0cd5794d6447297053668af2
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-03 22:54:25 -0700

    adds arrays map practice 6 solution

--- arrays_map/arrays_map_practice.js
+++ arrays_map/arrays_map_practice.js
@@ -36,15 +36,21 @@
 
 //  5. Start with an array of strings and create a new array with each string's length.
 //     For example, ["hello", "goodbye"] becomes [5, 7].
-var strings = ["hello", "goodbye"];
-var stringLength = [];
-strings.forEach(function(string) {
-  stringLength.push(string.length);
-});
-console.log(stringLength);
+// var strings = ["hello", "goodbye"];
+// var stringLength = [];
+// strings.forEach(function(string) {
+//   stringLength.push(string.length);
+// });
+// console.log(stringLength);
 
 //  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 //     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+var person = [{name: "Alice", age: 27}, {name: "Blane", age: 16}];
+var personAge = [];
+person.forEach(function(people) {
+  personAge.push(people.age);
+});
+console.log(personAge);
 
 //  7. Start with an array of numbers and create a new array with each number divided by 2.
 //     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
