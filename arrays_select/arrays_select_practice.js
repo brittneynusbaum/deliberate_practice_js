ed760ed23a238badb19003ebee73e4f0699f8702
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-09 23:22:11 -0700

    arrays select practice 10 solution

--- arrays_select/arrays_select_practice.js
+++ arrays_select/arrays_select_practice.js
@@ -99,14 +99,23 @@
 //  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 //     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-var items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
-var priceLessThanTen = [];
-items.forEach(function (item) {
-  if (item.price < 10) {
-    priceLessThanTen.push(item);
-  }
-});
-console.log(priceLessThanTen);
+// var items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
+// var priceLessThanTen = [];
+// items.forEach(function (item) {
+//   if (item.price < 10) {
+//     priceLessThanTen.push(item);
+//   }
+// });
+// console.log(priceLessThanTen);
 
 // 10. Start with an array of numbers and create a new array with only the odd numbers.
 //     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+var numbers = [2, 4, 5, 1, 8, 9, 7];
+var oddNumbers = [];
+numbers.forEach(function (number) {
+  if (number % 2 === 1) {
+    oddNumbers.push(number);
+  }
+});
+console.log(oddNumbers);
