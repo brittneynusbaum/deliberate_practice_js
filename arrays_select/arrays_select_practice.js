094cd328288a3c238a1f0062eaf43ae96f4fb422
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-09 23:07:54 -0700

    arrays select practice 4 solution

--- arrays_select/arrays_select_practice.js
+++ arrays_select/arrays_select_practice.js
@@ -24,18 +24,28 @@
 //  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 //     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
-var items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
-var priceGreaterFive = [];
+// var items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }];
+// var priceGreaterFive = [];
 
-items.forEach(function (item) {
-  if (item.price > 5) {
-    priceGreaterFive.push(item);
-  }
-});
-console.log(priceGreaterFive);
+// items.forEach(function (item) {
+//   if (item.price > 5) {
+//     priceGreaterFive.push(item);
+//   }
+// });
+// console.log(priceGreaterFive);
 
 //  4. Start with an array of numbers and create a new array with only the even numbers.
 //     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+var numbers = [2, 4, 5, 1, 8, 9, 7];
+var evenNumbers = [];
+
+numbers.forEach(function (number) {
+  if (number % 2 === 0) {
+    evenNumbers.push(number);
+  }
+});
+console.log(evenNumbers);
 
 //  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 //     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
